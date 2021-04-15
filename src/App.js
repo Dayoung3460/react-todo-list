2d6bba1d0991413373e8e4e0216a747a01ba5c9d
클라라 (김다영) <clara@coinvest.kr>
2021-04-15 20:42:05 +0900

    투두 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
+import Todo from "./components/Todo";
 
 function App() {
   const [inputText, setInputText] = useState('')
@@ -13,7 +14,7 @@ function App() {
         <h1>Todo List</h1>
       </header>
       <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
-      <TodoList />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Todo from './Todo'
 
-const TodoList = () => {
+const TodoList = ({todos, setTodos}) => {
     return (
         <div className='todo-container'>
             <ul className='todo-list'>
-
+                {todos.map(todo => (
+                    <Todo text={todo.text} key={todo.id} todos={todos} todo={todo} setTodos={setTodos}/>
+                ))}
             </ul>
         </div>
     )
